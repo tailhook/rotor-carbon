efcbf218ffc81c519984fd4ab6848c94a180c291
Paul Colomiets <paul@colomiets.name>
2016-02-01 17:51:53 +0200

    Eliminated warnings

--- src/fsm.rs
+++ src/fsm.rs
@@ -1,5 +1,4 @@
 use std::error::Error;
-use std::sync::{MutexGuard};
 
 use rotor::{Machine, Scope, Response, EventSet};
 use void::{Void, unreachable};
@@ -13,7 +12,7 @@ fn try_write(fsm: &mut Internal) {
             Ok(_) => continue,
             Err(e) => {
                 // TODO(tailhook) process error, reconnect, etc
-                panic!("Disconnected from carbon");
+                panic!("Disconnected from carbon: {}", e);
             }
         }
     }
@@ -22,28 +21,28 @@ fn try_write(fsm: &mut Internal) {
 impl<C> Machine for Fsm<C> {
     type Context = C;
     type Seed = Void;
-    fn create(seed: Self::Seed, scope: &mut Scope<Self::Context>)
+    fn create(seed: Self::Seed, _scope: &mut Scope<Self::Context>)
         -> Result<Self, Box<Error>>
     {
         unreachable(seed);
     }
-    fn ready(self, events: EventSet, scope: &mut Scope<Self::Context>)
+    fn ready(self, _events: EventSet, _scope: &mut Scope<Self::Context>)
         -> Response<Self, Self::Seed>
     {
         try_write(&mut self.0.lock().unwrap());
         Response::ok(self)
     }
-    fn spawned(self, scope: &mut Scope<Self::Context>)
+    fn spawned(self, _scope: &mut Scope<Self::Context>)
         -> Response<Self, Self::Seed>
     {
         Response::ok(self)
     }
-    fn timeout(self, scope: &mut Scope<Self::Context>)
+    fn timeout(self, _scope: &mut Scope<Self::Context>)
         -> Response<Self, Self::Seed>
     {
         Response::ok(self)
     }
-    fn wakeup(self, scope: &mut Scope<Self::Context>)
+    fn wakeup(self, _scope: &mut Scope<Self::Context>)
         -> Response<Self, Self::Seed>
     {
         try_write(&mut self.0.lock().unwrap());
