c99e9c0bd0f111b98a72aa4b1d2f658a7039e132
Paul Colomiets <paul@colomiets.name>
2016-02-04 13:29:13 +0200

    Add timestamped notifiers

--- src/sender.rs
+++ src/sender.rs
@@ -4,14 +4,27 @@ use time::get_time;
 use {Sender};
 
 impl<'a> Sender<'a> {
-    pub fn add_u64(&mut self, name: &str, value: u64) {
-        let ts = get_time().sec;
-        writeln!(self.0.buffer, "{} {} {}", name, value, ts).unwrap();
-    }
+    /// Send a generic convertible to i64
     pub fn add_int<I: Into<i64>=i64>(&mut self, name: &str, value: I) {
         let ts = get_time().sec;
         let val = value.into();
         writeln!(self.0.buffer, "{} {} {}", name, val, ts).unwrap();
+    }
+    /// The special case for u64, as it's the only type doesn't fit in i64
+    pub fn add_u64(&mut self, name: &str, value: u64) {
+        let ts = get_time().sec;
+        writeln!(self.0.buffer, "{} {} {}", name, value, ts).unwrap();
+    }
+    /// Send a generic convertible to i64 with a timestamp
+    pub fn add_int_at<I=i64>(&mut self, name: &str, value: I, ts: u64)
+        where I: Into<i64>
+    {
+        let val = value.into();
+        writeln!(self.0.buffer, "{} {} {}", name, val, ts).unwrap();
+    }
+    /// Send an u64 with a timestamp
+    pub fn add_u64_at(&mut self, name: &str, value: u64, ts: u64) {
+        writeln!(self.0.buffer, "{} {} {}", name, value, ts).unwrap();
     }
 }
 
